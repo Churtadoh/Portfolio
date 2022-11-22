71fda189ce748823511d34a2b549210c0a177125
Camilo <churtadoh92@gmail.com>
2022-11-22 17:43:11 -0500

    update cv link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,7 +16,7 @@ export default function Footer() {
                 justifyContent={'space-around'}>
                     <Grid item xs={12} sm={4}>
                         <Box borderBottom={2} width={{xs:'80%', md:250}} py={{xs: 1, sm: 1}} >
-                        <Link to="https://churtadoh.github.io/portfolio/files/CV%20Camilo%20Hurtado-Full%20Stack.pdf" target="_blank" download>
+                        <Link to="https://churtadoh.github.io/portfolio/files/CV%20Camilo%20Hurtado-Full%20Stack.pdf">
                             <Button sx ={{width:150}} variant="contained">
                              MY RESUME
                             </Button>
